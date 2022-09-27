5ebe3004c778560b96a0792fc535ff5e99e18136
amir ahmadi <amirahmadidev@gmail.com>
2022-09-28 01:12:37 +0330

    first version

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Amir Ahmadi - Software Engineer
+
+Amir is a software engineer and open source programmer. He started with PHP, When he came to a good understanding of programming , He realized that languages were just tools and turned more to programming concepts, And since then, he continues to learn new concepts of programming every day.
+
+    linkedin: https://www.linkedin.com/in/amir-ahmadi-dev/
+    Github:   https://github.com/saanny
+    Email:    AmirAhmadidev@gmail.com
+    
+`);
