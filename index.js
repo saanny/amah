a32ab3864c69972e996ddde69dc82542e2d85c91
amir ahmadi <amirahmadidev@gmail.com>
2022-09-28 01:26:47 +0330

    clear consoles

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
+const clearConsole = require("clear-any-console");
+
+clearConsole();
 console.log(`
 Amir Ahmadi - Software Engineer
 
-Amir is a software engineer and open source programmer. He started with PHP, When he came to a good understanding of programming , He realized that languages were just tools and turned more to programming concepts, And since then, he continues to learn new concepts of programming every day.
+Amir is a software engineer and open source programmer.
+He started with PHP, When he came to a good understanding of programming ,
+He realized that languages were just tools and turned more to programming concepts,
+And since then, he continues to learn new concepts of programming every day.
 
     linkedin: https://www.linkedin.com/in/amir-ahmadi-dev/
     Github:   https://github.com/saanny
